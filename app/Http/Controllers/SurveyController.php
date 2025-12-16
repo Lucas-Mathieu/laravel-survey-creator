c3e2a105d116469cbcd07022e18ccef0f0d2bc7a
Vivien <marcuz.vivien@gmail.com>
2025-12-16 10:47:38 +0100

    STORY-2: all done DTO controller for create modify & delete

--- app/Http/Controllers/SurveyController.php
+++ app/Http/Controllers/SurveyController.php
@@ -2,9 +2,21 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Actions\Survey\CreateSurveyAction;
+use App\Http\Requests\Survey\StoreSurveyRequest;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use App\DTOs\SurveyDTO;
 
 class SurveyController extends Controller
 {
-    //
+    public function store(StoreSurveyRequest $request, CreateSurveyAction $createSurvey): JsonResponse
+    {
+        $dto = SurveyDTO::fromRequest($request);
+        $survey = $createSurvey->handle($dto);
+
+        return response()->json([
+            'data' => $survey,
+            'message' => 'Survey created successfully.',
+        ], 201);
+    }
 }
