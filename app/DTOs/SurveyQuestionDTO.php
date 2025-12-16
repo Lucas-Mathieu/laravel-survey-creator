688af3b77a1f13cca624fdd70c83cdb00153fd77
antoine <antoine.vaslin@eud.esiee-it.fr>
2025-12-16 15:29:37 +0100

    première  modif poru story 3

--- app/DTOs/SurveyQuestionDTO.php
+++ app/DTOs/SurveyQuestionDTO.php
@@ -7,11 +7,17 @@ use Illuminate\Http\Request;
 final class SurveyQuestionDTO
 {
     private function __construct(
+        public readonly string $title,
+        public readonly string $questionType,
+        public readonly ?string $options,
     ) {}
 
-    public static function fromRequest(Request $request): self
+    public static function fromRequest(StoreQuestionRequest $request): self
     {
         return new self(
+            title: $request->input('title'),
+            questionType: $request->input('question_type'),
+            options: $request->input('options'),
         );
     }
 }
